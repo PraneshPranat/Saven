9f1a8cfd1f4c7487c21fbc6bb1f31e9e726946e6
Pranesh <pranesh2421@gmail.com>
2026-07-04 18:30:57 +0530

    add tailwindcss in project

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import tailwindcss from 'tailwindcss'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello world </h1>
+      
     </>
-  )
+  );
 }
 
 export default App
