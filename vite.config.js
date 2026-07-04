eb995c61153c61e559f88e6beeeb7d3559265503
Pranesh <pranesh2421@gmail.com>
2026-07-04 19:11:51 +0530

    congig crx-vite and manifest.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
+import { crx } from "@crxjs/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import manifest from "./manifest.json" assert { type: "json" };
+import react from "@vitejs/plugin-react";
 
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), crx({ manifest })],
+  build: {
+    rollupOptions: {
+      output: { chunkFileNames: "assets/chunk-[hash].js" },
+    },
+  },
 });
